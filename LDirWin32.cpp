adfa1a30bec2712cab7863f592b0890b33cd8a57
Valeryn4 <dtho47@yandex.ru>
2015-10-09 20:45:56 +1000

    LDir::mkpath FIX

--- LDirWin32.cpp
+++ LDirWin32.cpp
@@ -31,6 +31,10 @@ bool LDir::set_path(std::string path)
 	dir_path.clear();
 	dir_path.shrink_to_fit();
 	dir_path = path;
+	if (dir_path.at(dir_path.length() - 1) == '/' ||
+		dir_path.at(dir_path.length() - 1) == '\\')
+		dir_path.pop_back();
+	
 	if (exist(dir_path))
 		return true;
 	return false;
@@ -38,12 +42,10 @@ bool LDir::set_path(std::string path)
 
 bool LDir::cd(std::string path)
 {
-	std::cout << "cd " << path << std::endl;
 	if (!exist(dir_path))
 		return false;
 	if (path == "..") {
 		std::string temp = dir_path;
-		std::cout << "string '" << temp << "' leght: " << temp.length() << "size:  " << temp.size() << std::endl;
 		if (temp.at(temp.length()-1) == '/' || temp.at(temp.length()-1) == '\\')
 			temp.pop_back();
 		for (int i = temp.length()-1; i > 0; i--) {
@@ -139,46 +141,49 @@ bool LDir::exist(std::string path)
 
 bool LDir::mkdir(std::string name)
 {
+	std::string temp = name;
+	if (name.at(name.length() - 1) == '/' ||
+		name.at(name.length() - 1) == '\\')
+		name.pop_back();
+
 	if (!exist(dir_path))
 		return false;
 	if (dir_path.at(dir_path.length() - 1) == '/' ||
 		dir_path.at(dir_path.length() - 1) == '\\')
 		dir_path.pop_back();
 	if (name.at(0) == '/' || name.at(0) == '\\') {
-		name.clear();
 		name = dir_path + name;
 	}
 	else {
-		name.clear();
 		name = dir_path + '/' + name;
 
 	}
 
-	name.clear();
-	name = dir_path + '/' + name;
-
 	if (CreateDirectory(name.c_str(), NULL) ||
-		ERROR_ALREADY_EXISTS == GetLastError())
+		ERROR_ALREADY_EXISTS == GetLastError()) {
+		cd('/' + temp);		
 		return true;
+	}
 	return false;
 }
 
-bool LDir::mkpath(std::string name)
+bool LDir::mkpath(std::string path)
 {
+	std::cout << "mkpath " << path << std::endl;
 	std::string temp_path;
 	std::string temp_dir;
 		temp_path.clear();
 		temp_dir.clear();
-	int n = name.length();
-	int dir = 0;
+	int n = path.length();
 	bool temp_exist = false;
 	//brain hack
 	for (int i = 0; i < n; i++) {
 		if (temp_exist == true)
-			temp_dir.push_back(name.at(i));
-		if (name.at(i) == '/' || name.at(i) == '\\') {
+			temp_dir.push_back(path.at(i));
+
+		if (path.at(i) == '/' || path.at(i) == '\\' || i == n-1) {
 			if (temp_exist == false) {
-				dir++;
+				set_path(temp_path);
 				temp_exist = true;
 			}
 			else if (temp_exist == true) {
@@ -186,11 +191,13 @@ bool LDir::mkpath(std::string name)
 					if (!mkdir(temp_dir))
 						return false;
 				}
+
 				temp_dir.clear();
-				temp_exist = false;
+				set_path(temp_path);
+
 			}
 		}
-		temp_path.push_back(name.at(i));
+		temp_path.push_back(path.at(i));
 	}
 	//end brain hack;
 
--- main.cpp
+++ main.cpp
@@ -16,6 +16,18 @@ int main() {
 	dir->cd("Users");
 	std::cout << dir->get_path() << std::endl;
 
+	dir->cd("..");
+	std::cout << dir->get_path() << std::endl;
+
+	dir->cd("SQL");
+	std::cout << dir->get_path() << std::endl;
+
+	dir->mkdir("testdir");
+	std::cout << dir->get_path() << std::endl;
+
+	dir->mkpath("C:/SQL/Test1/test2/test3");
+	std::cout << dir->get_path() << std::endl;
+
 	dir->exist("C:/Windows");
 	
 	delete o1;
